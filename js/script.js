8c1bdf253c53916ba7974856b6949d0a6d9e371b
Zosia Samosia <Zosia@Samosia.pl>
2023-05-08 12:44:50 +0200

    qol

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict'
+console.log(1)
+const btn=document.querySelector('header a')
+const h1=document.querySelector('header h1')
+const h1text=h1.innerText
+let counter=0
+btn.addEventListener('click',function(){
+    counter++
+    // if(counter%2==0)
+    //     h1.innerText=h1text
+    // else
+    //     h1.innerText='Bedzie pan zadowolony'
+    counter%2==0 ? h1.innerText=h1text : h1.innerText='Bedzie pan zadowolony'
+
+})
+console.log(2)
